f51c1b892b3088ec6534214f57896105fee54c4d
ElSicopata <59068392+ElSicopata@users.noreply.github.com>
2021-09-06 19:14:49 +0000

    handler de comandos y eventos!

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//-----------------------------------------------------//
+//-----------------------24/7 REPLIT------------------------------//
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -10,59 +10,34 @@ app.get('/', function(request, response) {
 });
 app.listen(3000, () => console.log(`FUNCIONAMIENTO CORRECTO`));
 
-
-//-------------------------FS(HANDLER Y MAS)---------------------------//
-
-const fs = require('fs');
-
-
-//-------------------------DISCORD---------------------------//
-
-const { Client , Intents} = require('discord.js');
-
-const client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
-
-//------------------------PRESENCIA DEL BOT--------------------------//
-
-client.on("ready", () => {
-   function presence() {
-   client.user.setPresence({
-     status: 'on',
-     activity: {
-     name: 'Trick X Human server',
-     type: 'WATCHING'
-     }
-   })
-   }
-   presence();
-   console.log(`INICIADO COMO BOT: ${client.user.tag}`); 
-});
-
-
 //-------------------------- CONST EXTRAS ---------------------------//
 const Monitor = require('ping-monitor');
 
-require('dotenv').config();
-
 const { BOT } = require('./src/config')
+
+const { Client , Intents, MessageEmbed, Collection, Util } = require('discord.js');
+
+const client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
+
+//-------------------------FS HANDLER Y MAS---------------------------//
+
+const fs = require('fs');
+
+const path = require('path');
+
+const { comandos } = require(`${__dirname}/src/utils/handler/comandos.js`);
+
+const { eventos } = require(`${__dirname}/src/utils/handler/eventos.js`);
+
+comandos(fs, client, Collection)
+eventos(fs, client, MessageEmbed, Util)
+
+
+//------------------------PRESENCIA DEL BOT--------------------------//
 
 //---------------------------- CODIGO DEL BOT ----------------------------//
 
-client.on('messageCreate', (message) => {
-    if(message.author.bot) return;
-    if(!message.content.startsWith(BOT.prefix)) return;
-    
-    let [cmdname, ...cmdargs]= message.content.slice(BOT.prefix.length).trim().split(/\s + /);
-    
-    if(cmdname === 'ping'){
-        message.channel.send('pong')
-    }
-    
-
-})
 
 //-------------------------TOKEN---------------------------//
-
-const mySecret = process.env['TOKEN']
 
 client.login(BOT.token);
